a5710b0499ce93fa5f2ab6c9bdaec72eb8817648
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-04-07 22:07:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
-<?php
-	require './header.php';
-	$m = new model();
-	$result = $m->get_main_page_blog_data();
-?>
+<?php require './header.php'; ?>
+
 	<div class="container">
-		<?php
-			if ($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
-		?>
-		<div class="jumbotron">
-			<h2 class="display-4"><?php echo $row["HEADING"]; ?></h2>
-			<p class="lead"><?php echo $row["DESCRIPTION"]; ?></p>
-			<p class="lead">published by <?php echo $row["OWNER"]. " on " .  $row["PUBLISH_DATE"]; ?></p>
-			
+		<div class="row">
+			<div class="col-sm-10">
+			<?php
+				$m = new model();
+				$result = $m->get_main_page_blog_data();
+				if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+			?>
+			<div class="jumbotron">
+				<h2 class="display-4"><?php echo $row["HEADING"]; ?></h2>
+				<p class="lead"><?php echo $row["DESCRIPTION"]; ?></p>
+				<p class="lead">Author: <?php echo $row["OWNER"]; ?></p>
+				<p class="lead">Published on: <?php echo $row["PUBLISH_DATE"]; ?></p>
+			</div>
+			<?php }
+				}else{
+			?>
+			<h1>NO BLOGS TO DISPLAY!</h1>
+			<?php } ?>
+			</div>
+			<div class="col-sm-2">
+			<?php 
+				$type = $_SESSION['user_type'];
+				if( $type == "STUDENT"){
+			?>
+				<a href="lost_found.php" class="btn btn-primary" target="_blank">LOST FOUND</a>
+			<?php } ?>
+			</div>
 		</div>
-		<?php }
-			}else{
-		?>
-		<h1>NO BLOGS TO DISPLAY!</h1>
-		<h4>Start blogging today!</h4>
-		<?php } ?>
 	</div>
+
+<?php require './footer.php'; ?>
