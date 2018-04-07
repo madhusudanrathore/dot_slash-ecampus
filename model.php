0e6294371cc6216583ba13f66ca6bcde70b18a8d
nidhi0710 <36001038+nidhi0710@users.noreply.github.com>
2018-04-07 15:03:04 +0530

    Update model.php

--- model.php
+++ model.php
@@ -6,20 +6,21 @@
 	        	$db = new dbconnection();
 	        	$this->conn = $db->connect_to_db();
 		}
-		function get_main_page_blog_data( ){
-			$get_data_query = "SELECT * FROM main_page_blog_table ORDER BY PUBLISH_DATE DESC";
-			$result = $this->conn->query($get_data_query);
-			return $result;
-		}
-		function get_achievement_data( ){
-			$get_data_query = "SELECT * FROM main_page_blog_table WHERE NOTICE_CATEGORY='ACHIEVEMENT' ORDER BY PUBLISH_DATE DESC";
-			$result = $this->conn->query($get_data_query);
-			return $result;
-		}
-		function log_out(){
-			session_unset();
-			session_destroy();
-			mysqli_close($this->conn);
+		function register_user( $name_param, $email_param, $contact_param, $password_param ){
+			/*MAKE A NEW ENTRY RECORD FOR USER*/
+			$new_user_query = "INSERT INTO user_table (NAME, EMAIL, CONTACT_NUMBER, PASSWORD) VALUES ('$name_param', '$email_param', '$contact_param', '$password_param')";
+			/*MAKE A NEW TABLE TO STORE USER'S BLOGS*/
+			$new_usertable_query = "CREATE TABLE `$email_param` ( HEADING VARCHAR(50) , DESCRIPTION VARCHAR(500) NOT NULL, PUBLISH_DATE TIMESTAMP )";
+
+			if ($this->conn->query($new_usertable_query) === TRUE) {
+			    if ($this->conn->query($new_user_query) === TRUE) {
+				    $_SESSION['user_email'] = $email_param;
+				} else {
+				    echo "Error: " . $new_user_query . " " . $conn->error;
+				}
+			} else {
+			    echo "Error creating table: " . $this->conn->error;
+			}
 		}
 		function login_user( $email_param, $password_param ){
 			$login_query = "SELECT * FROM user_table WHERE EMAIL='$email_param'";
@@ -36,6 +37,21 @@
 					echo "INVALID LOGIN PASSWORD";
 				}
 			}
+		}
+		function get_main_page_blog_data( ){
+			$get_data_query = "SELECT * FROM main_page_blog_table ORDER BY PUBLISH_DATE DESC";
+			$result = $this->conn->query($get_data_query);
+			return $result;
+		}
+		function get_achievement_data( ){
+			$get_data_query = "SELECT * FROM main_page_blog_table WHERE NOTICE_CATEGORY='ACHIEVEMENT' ORDER BY PUBLISH_DATE DESC";
+			$result = $this->conn->query($get_data_query);
+			return $result;
+		}
+		function log_out(){
+			session_unset();
+			session_destroy();
+			mysqli_close($this->conn);
 		}
 	}
 ?>
