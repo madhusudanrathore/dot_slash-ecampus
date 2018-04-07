19ad5d22b762e8b45dd982f861629363dbcb326a
nidhi0710 <36001038+nidhi0710@users.noreply.github.com>
2018-04-07 17:25:51 +0530

    Update model.php

--- model.php
+++ model.php
@@ -6,15 +6,17 @@
 	        	$db = new dbconnection();
 	        	$this->conn = $db->connect_to_db();
 		}
-		function register_user( $name_param, $email_param, $contact_param, $password_param ){
+		function register_user( $name_param, $email_param,$dept_param,$contact_param, $password_param ,$user_type_param,$parent_contact_param){
 			/*MAKE A NEW ENTRY RECORD FOR USER*/
-			$new_user_query = "INSERT INTO user_table (NAME, EMAIL, CONTACT_NUMBER, PASSWORD) VALUES ('$name_param', '$email_param', '$contact_param', '$password_param')";
+			$new_user_query = "INSERT INTO user_table (NAME, EMAIL,DEPARTMENT, CONTACT_NUMBER, PASSWORD, USER_TYPE, PARENT_CONTACT) VALUES ('$name_param', '$email_param','$dept_param', '$contact_param', '$password_param','$user_type_param','$parent_contact_param')";
 			/*MAKE A NEW TABLE TO STORE USER'S BLOGS*/
 			$new_usertable_query = "CREATE TABLE `$email_param` ( HEADING VARCHAR(50) , DESCRIPTION VARCHAR(500) NOT NULL, PUBLISH_DATE TIMESTAMP )";
 
 			if ($this->conn->query($new_usertable_query) === TRUE) {
 			    if ($this->conn->query($new_user_query) === TRUE) {
 				    $_SESSION['user_email'] = $email_param;
+				    $_SESSION['user_name'] = $name_param;
+				    $_SESSION['user_type'] = $user_type_param;
 				} else {
 				    echo "Error: " . $new_user_query . " " . $conn->error;
 				}
@@ -29,10 +31,14 @@
 			$row = $result->fetch_assoc();
 			$user_entered_password = $password_param;
 			$original_password = $row["PASSWORD"];
-
+			$user_name = $row["NAME"];
+			$user_type = $row["USER_TYPE"];
+			
 			if ($result->num_rows > 0) {
 				if( $user_entered_password == $original_password ){
 					$_SESSION['user_email'] = $email_param;
+					$_SESSION['user_name'] = $user_name;
+					$_SESSION['user_type'] = $user_type;
 				}else{
 					echo "INVALID LOGIN PASSWORD";
 				}
