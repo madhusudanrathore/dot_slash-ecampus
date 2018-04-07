92dca38c6ee44378fd8a9d9a0c90a1a0fa4488fe
Madhusudan Rathore <rmadhusudan359@gmail.com>
2018-04-08 05:16:25 +0530

    Add files via upload

---
+++ utility_display.php
@@ -0,0 +1,9 @@
+			<div class="col-md-2">
+				<p class="lead" style="color: #808080; border-bottom: #0EA300 solid 2px;">Also explore:</p>
+					<a href="#" class="btn btn-success" role="button">LOST AND FOUND</a><br><br>
+					<a href="#" class="btn btn-success" role="button">CANTEEN</a><br><br>
+					<a href="#" class="btn btn-success" role="button">E-LIBRARY</a><br><br>
+					<a href="#" class="btn btn-success" role="button">CLUBS</a><br><br>
+					<a href="#" class="btn btn-success" role="button">CAMPUS OLX</a><br><br>
+					<a href="#" class="btn btn-success" role="button">MONTHLY REPORT</a>
+			</div>
