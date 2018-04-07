4dd736e1ddb7399da760d6ad72d14b54bd888fb4
nidhi0710 <36001038+nidhi0710@users.noreply.github.com>
2018-04-07 17:39:52 +0530

    Update model.php

--- model.php
+++ model.php
@@ -9,20 +9,14 @@
 		function register_user( $name_param, $email_param,$dept_param,$contact_param, $password_param ,$user_type_param,$parent_contact_param){
 			/*MAKE A NEW ENTRY RECORD FOR USER*/
 			$new_user_query = "INSERT INTO user_table (NAME, EMAIL,DEPARTMENT, CONTACT_NUMBER, PASSWORD, USER_TYPE, PARENT_CONTACT) VALUES ('$name_param', '$email_param','$dept_param', '$contact_param', '$password_param','$user_type_param','$parent_contact_param')";
-			/*MAKE A NEW TABLE TO STORE USER'S BLOGS*/
-			$new_usertable_query = "CREATE TABLE `$email_param` ( HEADING VARCHAR(50) , DESCRIPTION VARCHAR(500) NOT NULL, PUBLISH_DATE TIMESTAMP )";
-
-			if ($this->conn->query($new_usertable_query) === TRUE) {
-			    if ($this->conn->query($new_user_query) === TRUE) {
+			if ($this->conn->query($new_user_query) === TRUE) {
 				    $_SESSION['user_email'] = $email_param;
 				    $_SESSION['user_name'] = $name_param;
 				    $_SESSION['user_type'] = $user_type_param;
 				} else {
 				    echo "Error: " . $new_user_query . " " . $conn->error;
 				}
-			} else {
-			    echo "Error creating table: " . $this->conn->error;
-			}
+			
 		}
 		function login_user( $email_param, $password_param ){
 			$login_query = "SELECT * FROM user_table WHERE EMAIL='$email_param'";
