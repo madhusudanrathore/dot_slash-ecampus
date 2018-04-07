3d7edb74dfa7ac76f22f318bca61b786e71818d3
nidhi0710 <36001038+nidhi0710@users.noreply.github.com>
2018-04-07 17:25:03 +0530

    Update register.php

--- register.php
+++ register.php
@@ -9,7 +9,7 @@
 
 	<div class="jumbotron jumbotron-fluid" style="text-align: center;">
 		<h1 class="display-4">Welcome to E-LDCE!</h1>
-		<p class="lead">Making academics digital and easier</p>
+		<p class="lead">Making academics digital easier</p>
 		<?php if(isset($_SESSION['user_email'])) { ?>
 		<form method="POST" action="./controller.php">
 			<span style="float: right;">Hello! <b><?php echo $_SESSION['user_name']; ?></b></span>
@@ -27,16 +27,17 @@
 					<div class="form-group" style="text-align: center;">
 						<input type="text" class="form-control" name="user_name" placeholder="enter name">
 						<input type="email" class="form-control" name="user_email" placeholder="enter email">
+						<input type="text" class="form-control" name="user_dept" placeholder="enter department">
 						<input type="number" class="form-control" name="user_contact_number" placeholder="enter contact number">
 						<input type="password" class="form-control" name="user_password" placeholder="password">
 					</div>
-					<p>Select Registeration type</p>
+					<p>Select your type</p>
 					<input type="radio" id="teacher" name="user_type" value="AUTHORITY">College Authority
-  					<input type="radio" id="student" name="user_type" value="STUDENT">Student
+  					<input type="radio" id="student" name="user_type" value="STUDENT" checked>Student
   					<input type="number" id="parent_number" class="form-control" name="parent_contact_number" placeholder="enter parent's contact number">
   					<br/><br/>
 					<input type="submit" class="btn btn-primary" name="register_btn" value="Register">
-					<input type="reset" class="btn btn-secondary" value="Cancel">
+					<input type="reset" class="btn btn-secondary" value="Reset Form">
 				</form>
 			</div>
 			<div class="col-sm-6">
@@ -47,7 +48,7 @@
 						<input type="password" class="form-control" name="user_password" placeholder="password">
 					</div>
 					<input type="submit" class="btn btn-success" name="log_in_btn" value="Log In">
-					<input type="reset" class="btn btn-secondary" value="Cancel">
+					<input type="reset" class="btn btn-secondary" value="Reset">
 				</form>
 			</div>
 		</div>
@@ -64,5 +65,3 @@
 			});
 		});
 	</script>
-
-<?php require './footer.php'; ?>
