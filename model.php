66b5f648d317ff24b89a5f9cbfa2a95e69045501
nidhi0710 <36001038+nidhi0710@users.noreply.github.com>
2018-04-07 13:35:53 +0530

    Update model.php

--- model.php
+++ model.php
@@ -1,25 +1,21 @@
 <?php
 	require './dbconnection.php';
-
 	class model{
 	    private $conn;
 	    function __construct(){
 	        	$db = new dbconnection();
 	        	$this->conn = $db->connect_to_db();
 		}
-
 		function get_main_page_blog_data( ){
 			$get_data_query = "SELECT * FROM main_page_blog_table ORDER BY PUBLISH_DATE DESC";
 			$result = $this->conn->query($get_data_query);
 			return $result;
 		}
-
 		function get_achievement_data( ){
-			$get_data_query = "SELECT * FROM main_page_blog_table ORDER BY PUBLISH_DATE DESC WHERE NOTICE_CATEGORY='ACHIEVEMENT'";
+			$get_data_query = "SELECT * FROM main_page_blog_table WHERE NOTICE_CATEGORY='ACHIEVEMENT' ORDER BY PUBLISH_DATE DESC";
 			$result = $this->conn->query($get_data_query);
 			return $result;
 		}
-
 		function log_out(){
 			session_unset();
 			session_destroy();
