266a9fcb35988ba4f8500350c33971968ad590db
nidhi0710 <36001038+nidhi0710@users.noreply.github.com>
2018-04-08 14:39:06 +0530

    Update query.php

--- query.php
+++ query.php
@@ -1,12 +1,17 @@
 <?php require './header.php'; ?>
+	
+	<div class="container">
+
+		<a class="btn btn-danger" id="input_query">Have a Query?</a>
+		<a href="department.php" class="btn btn-success" id="view_query">View Query</a>
+
 
 	<div class="container">
-		<h3 style="color: #0EA300; border-bottom: #0EA300 solid 2px;">ASK YOUR DOUBTS HERE:</h3>
-		<div class="jumbotron">
 		<form method="POST" action="./controller.php" enctype="multipart/form-data">
+		<div id="write_query">
 			<div class="form-group">
-				Topic of your doubt: <input type="text" name="query_heading" placeholder="heading goes here"><br /><br />
-				Describe your doubt: <textarea rows="10" cols="50" name="query_content" placeholder="content goes here"></textarea><br><br>
+				<input type="text" name="query_heading" placeholder="heading goes here"><br /><br />
+				<textarea rows="10" cols="50" name="query_content" placeholder="content goes here"></textarea>
 				Select image to upload:
     			<input type="file" class="btn btn-info" name="img" id="fileToUpload"><br>
 			</div>
@@ -20,11 +25,34 @@
   			<input type="radio" name="department_type" value="EE">Electrical<br />
   			<br /><br />
 			<input type="hidden" name="query_id">
+			<p>Request doubt live solving</p>
+			<input type="radio" name="live" id="yes" value="yes" >Yes<br />
+  			<input type="radio" name="live" id="no" value="no" checked>No<br />
+  			<div id="request_live">
+  			<input type="number" name="std_no" class="form-control" value="<?php echo NULL?>" placeholder="number of attending students" ><br/>
+  			<input type="text" name="prof_id" class="form-control" value="<?php echo NULL?>" placeholder="write email-id of professor"><br/>
+  			</div>
   			<!-- SUBMIT -->
 			<input type="submit" class="btn btn-success" name="new_query_btn" value="Post">
 			<input type="reset" class="btn btn-danger" name="cancel_query_btn" value="Cancel">
 		</form>
 	</div>
-	</div>
-
+</div>
+<script type="text/javascript">
+		$(document).ready(function () {
+			$("#request_live").hide();
+			$("#yes").click(function () {
+				 $("#request_live").show();
+			});
+			$("#no").click(function () {
+				 $("#request_live").hide();
+			});
+		});
+		$(document).ready(function () {
+			$("#write_query").hide();
+			$("#input_query").click(function () {
+				 $("#write_query").show();
+			});
+		});
+	</script>
 <?php require './footer.php'; ?>
